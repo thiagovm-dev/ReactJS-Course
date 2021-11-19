b940fc33ac5bff09d107c409b708686aa067471e
thiago viana <tvm.dev2015@gmail.com>
2021-11-18 22:11:30 -0300

    Class16 react-icons and Course complete

--- matheus/src/components/layout/Footer.js
+++ matheus/src/components/layout/Footer.js
@@ -1,7 +1,17 @@
+import { FaFacebook, FaInstagram, FaLinkedin, FaYoutube } from 'react-icons/fa'
+import styles from './Footer.module.css'
 
 
 function Footer() {
-    return <footer>Footer</footer>
+    return <footer>
+        <ul className={styles.social_list} >
+        <li><FaFacebook /></li>
+        <li><FaInstagram /></li>
+        <li><FaLinkedin /></li>
+        <li><FaYoutube /></li>
+        </ul>
+        <p>Our Footer</p>
+        </footer>
        
     
 }
